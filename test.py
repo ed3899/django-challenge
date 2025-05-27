c2ae6a259ca201595066f37517c265879042185d
Eduardo Casanova <53999059+ed3899@users.noreply.github.com>
2025-05-27 15:39:16 +0000

    check

--- test.py
+++
@@ -1,7 +0,0 @@
-import os
-
-for (root,dirs,files) in os.walk('/workspaces/django-challenge/docs-sm', topdown=True):
-    print (f"root: {root}")
-    print (f"dirs: {dirs}")
-    print (files)
-    print ('--------------------------------')
